bbfbbac10f40ccba5b292b6183b2c15fe6f20ded
qfmtgalon <faithmarygalon.fmg@gmail.com>
2023-01-16 22:23:11 +0800

    Welcome page layout

--- app/src/main/java/com/example/happytree/LandingPage.kt
+++ app/src/main/java/com/example/happytree/LandingPage.kt
@@ -1,7 +1,13 @@
 package com.example.happytree
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.happytree.databinding.ActivityLandingPageBinding
 
 class LandingPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
